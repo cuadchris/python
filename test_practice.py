49b76888b437191653608cc3148c15248bb37dfc
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-04-14 09:52:48 -0400

    assert

--- test_practice.py
+++ test_practice.py
@@ -7,6 +7,7 @@ class TestSum(unittest.TestCase):
         self.assertEqual(sum([5,5,6]), 16)
         self.assertEqual(sum([10,10,10]), 30)
         self.assertEqual(sum([20,10,10]), 40)
+        self.assertEqual(max(30, 50, 20), 50)
 
 if __name__ == '__main__':
     unittest.main()
